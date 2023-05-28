c124598cc6ad48e6288fd6f28591fdad293713a5
aissatheprogrammer <mabroumi03@gmail.com>
2023-05-28 11:48:14 +0200

    second version of portfolio

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+    width: {
+      "width-image-cadre": "803px",
+      "width-title-box": "536.71px",
+    },
+    height: { "width-image-cadre": "460px", "height-description-box": "556px" },
+  },
+  plugins: [],
+};
